d90fcc168813181a911e78dd90378d504a5f7f56
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-11-29 16:03:19 +0100

    lexer: Added determinization

--- parser/lexer.jl
+++ parser/lexer.jl
@@ -10,6 +10,7 @@ end
 mutable struct LState
     id
     # Associates a token or epsilon to a state
+    # Note that before determinizing ctx, it gathers states, not ints
     transitions
     # Whether we can accept the input in this state
     terminal
@@ -18,11 +19,13 @@ end
 lctx_new() = LCtx(0, [])
 
 function lstate_new!(ctx; transitions = [], terminal = false)
-    s = LState(ctx.state_id += 1, transitions, terminal)
+    s = lstate_create(ctx.state_id += 1, transitions, terminal)
     push!(ctx.states, s)
 
     return s
 end
+
+lstate_create(id = 0, transitions = [], terminal = false) = LState(id, transitions, terminal)
 
 # Links a to b by the token tok (can be empty)
 function lstate_link!(a, tok, b)
@@ -35,8 +38,9 @@ Base.hash(a::LState) = hash(a.id)
 function Base.print(io::IO, s::LState)
     println(io, "LState(")
     println(io, "    id = $(s.id)")
+    println(io, "    terminal = $(s.terminal)")
     for (tok, state) in s.transitions
-        println(io, "    '$tok' -> $(state.id)")
+        println(io, "    '$tok' -> $(isa(state, LState) ? state.id : state)")
     end
 
     print(io, ")")
@@ -77,10 +81,12 @@ end
 #     end
 # end
 
+# !!! The initial state must be the first created (with id 1)
 function determinize!(ctx)
     # Epsilon closures
-    closures = [[] for _ in 1:length(ctx.states)]
+    closures = [Set() for _ in 1:length(ctx.states)]
 
+    # eps-NFA to NFA
     for (i, s) in enumerate(ctx.states)
         # Bfs
         visited = Set([s])
@@ -97,29 +103,126 @@ function determinize!(ctx)
                 end
             end
         end
-
-        println(closures[i])
     end
+
+    # NFA to DFA
+    new_states = [Set([1])]
+    visited_states = Set()
+    states = Set([])
+    init_state = nothing
+    while length(new_states) != 0
+        # Now an id is a set of int ids
+        state_ids = pop!(new_states)
+        push!(visited_states, state_ids)
+
+        transitions = Dict()
+        terminal = false
+
+        for state_id in state_ids
+            state = ctx.states[state_id]
+
+            # This state contains a terminal state
+            if state.terminal
+                terminal = true
+            end
+
+            for (tok, next_state) in state.transitions
+                # Ignore epsilons
+                if tok == ""
+                    continue
+                end
+
+                # TODO : Can have multiple transitions same token
+                next_state_closure = closures[next_state.id]
+
+                if !haskey(transitions, tok)
+                    transitions[tok] = next_state_closure
+                else
+                    union!(transitions[tok], next_state_closure)
+                end
+            end
+        end
+
+        # Parse new states
+        for (tok, next_state_ids) in transitions
+            # If not parsed, add it to the stack
+            if next_state_ids ∉ visited_states
+                push!(new_states, next_state_ids)
+            end
+        end
+
+        state = lstate_create(state_ids, transitions, terminal)
+
+        if length(visited_states) != 1
+            push!(states, state)
+        else
+            init_state = state
+        end
+    end
+
+    # Convert states to linear and put init_state at the first position
+    states = [init_state, states...]
+
+    # Change states ids
+    stateids2stateid = Dict()
+    for (i, s) in enumerate(states)
+        stateids2stateid[s.id] = i
+    end
+
+    for s in states
+        s.id = stateids2stateid[s.id]
+        for (tok, nstate) in s.transitions
+            s.transitions[tok] = stateids2stateid[nstate]
+        end
+    end
+
+    ctx.states = states
 end
 
 # --- Main ---
 ctx = lctx_new()
-sinit = lstate_new!(ctx)
-# sa = ltok!(ctx, "a")
-# sb = ltok!(ctx, "b")
-# sc = ltok!(ctx, "c")
-sa = lstate_new!(ctx)
-sb = lstate_new!(ctx)
-# sc = lstate_new!(ctx)
-lstate_link!(sa, "A", sb)
-send = lend!(ctx)
 
-llink!(sinit, sa)
-llink!(sb, send)
-llink!(send, sa)
-llink!(sa, send)
+# https://www.youtube.com/watch?v=dlH2pIndNrU
+s1 = lstate_new!(ctx)
+s2 = lstate_new!(ctx)
+s3 = lstate_new!(ctx)
+s4 = lstate_new!(ctx)
+s5 = lstate_new!(ctx, terminal=true)
+
+lstate_link!(s1, "x", s2)
+lstate_link!(s1, "z", s5)
+lstate_link!(s3, "x", s4)
+lstate_link!(s3, "y", s4)
+llink!(s2, s3)
+llink!(s2, s5)
+llink!(s4, s5)
+llink!(s4, s3)
+
+for s in ctx.states
+    println(s)
+end
+
+println("---")
 
 determinize!(ctx)
+
+
+# sinit = lstate_new!(ctx)
+# # sa = ltok!(ctx, "a")
+# # sb = ltok!(ctx, "b")
+# # sc = ltok!(ctx, "c")
+# sa = lstate_new!(ctx)
+# sb = lstate_new!(ctx)
+# # sc = lstate_new!(ctx)
+# lstate_link!(sa, "A", sb)
+# send = lend!(ctx)
+
+# llink!(sinit, sa)
+# llink!(sb, send)
+# llink!(send, sa)
+# llink!(sa, send)
+
+# determinize!(ctx)
 
 for s in ctx.states
     println(s)
