fd4ea5403b5a59df51386cd3b1546970b3b50919
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-12-06 15:25:03 +0100

    parser: Updated syntax to recognize semantics

--- parser/syntax.jl
+++ parser/syntax.jl
@@ -1,14 +1,23 @@
+# Used to parse syntax files
+
+include("lexerparser.jl")
+
 # Parses all lines of the syntax file
 # Returns all tokens, prods, etc...
 # necessary for lexing and parsing
 function parsesyntax(lines)
     # lexer or parser
     section = nothing
+
+    # Parse only strings now
     lexer_items = []
     parser_items = []
+
+    # Fetch names of tokens
     terminals = []
     nonterminals = []
 
+    # Parse all lines
     for (lno, l) in enumerate(lines)
         l = strip(l)
         if length(l) == 0
@@ -29,37 +38,73 @@ function parsesyntax(lines)
         else
             @assert section != nothing "Code outside of section at line $lno"
 
-            # TODO : Parse terminals...
             if section == "lexer"
                 arrow = findfirst("->", l)
                 colon = findfirst(r"[^\\]:", l)
                 colon = colon.stop
-                name, reg, rule = strip(l[begin:arrow.start - 1]), strip(l[arrow.stop + 1:colon - 1]), strip(l[colon + 1:end])
 
-                println("Lexer |$name| |$reg| |$rule|")
+                name, rule = strip(l[begin:arrow.start - 1]), strip(l[colon + 1:end])
+                reg = strip(l[arrow.stop + 1:colon - 1])[2:end - 1]
+
+                @assert name ∉ nonterminals "Can't produce non terminal $name from lexer (line $lno)"
+                push!(terminals, name)
+
+                # println("Lexer |$name| |$reg| |$rule|")
                 push!(lexer_items, (name, reg, rule))
             else
-                # arrow = findfirst("->", l)
-                # colon = findfirst(':', l)
-                # name, reg, rule = strip(l[begin:arrow - 1]), strip(l[arrow + 2:colon - 1]), strip(l[colon + 1:end])
+                arrow = findfirst("->", l)
+                colon = findfirst(r"[^\\]:", l)
+                colon = colon.stop
 
-                # println("Parser |{name}| |{reg}| |{rule}|")
-                # push!(parser_items, (name, reg, rule))
+                name = strip(l[begin:arrow.start - 1])
+                prod = [strip(x) for x in split(strip(l[arrow.stop + 1:colon - 1]), ' ')]
+                rule = strip(l[colon + 1:end])
+
+                @assert name ∉ terminals "Can't produce terminal $name from parser (line $lno)"
+                push!(nonterminals, name)
+
+                # println("Parser |$name| |$prod| |$rule|")
+                push!(parser_items, (lno, name, prod, rule))
             end
         end
     end
 
+    # Lexer
+    regs = lexer_items
+
+    # Create tokens
+    terminals = [tok_new(name, terminal=true) for name in terminals]
+    nonterminals = [tok_new(name, terminal=false) for name in nonterminals]
+    tokens = vcat(terminals, nonterminals)
+    id2tok = Dict([t.id => t for t in tokens])
+
+    # Create prods and its rules
     prods = []
     produce_rules = []
+    for (lno, left_name, prod, produce_rule) in parser_items
+        local left_tok, prod
 
-    # # TODO : Construct
-    # tokens = vcat(terminals, nonterminals)
+        try
+            left_tok = id2tok[left_name]
+            prod = [id2tok[t] for t in prod]
+        catch e
+            error("Invalid rule at line $lno, unknown token ($e)")
+        end
 
-    # return (regs, tokens, prods, produce_rules)
+        push!(prods, prod_new(left_tok, prod))
+        push!(produce_rules, produce_rule)
+    end
+
+    return (regs, tokens, prods, produce_rules)
 end
 
 file = "calculator.syntax"
 open(file) do io
     lines = readlines(io)
-    parsesyntax(lines)
+    (regs, tokens, prods, produce_rules) = parsesyntax(lines)
+
+    println("regs = $regs")
+    println("tokens = $tokens")
+    println("prods = $prods")
+    println("produce_rules = $produce_rules")
 end
